8a6b7be6b550d5da138f9734cee1b7dbf4482db7
dariorojas6 <dario43055@gmail.com>
2023-03-31 17:57:19 -0300

    pedido a la api

--- src/components/weatherApp.js
+++ src/components/weatherApp.js
@@ -1,3 +1,30 @@
-export default function WeatherApp(){
-    return <div>Hola</div>
+import { useState } from "react"
+import WeatherForm from "./weatherForm.jsx";
+const { REACT_APP_KEY, REACT_APP_URL } = process.env;
+
+export default function WeatherApp() {
+    const [weather, setWeather] = useState(null);
+
+    async function loadInfo(city = 'london') {
+        try {
+            const request = await fetch(`${REACT_APP_URL}&key=${REACT_APP_KEY}&q=${city}`);
+            const json = await request.json();
+
+            console.log(json)
+        } catch (error) {
+
+        }
+    }
+
+    function handleChangeCity(city) {
+        setWeather(null);
+        loadInfo(city);
+
+    }
+    return (
+        <div>
+            <WeatherForm onChangeCity={handleChangeCity} />
+            <div>Info</div>
+        </div>
+    );
 }
