de58ee35b49ab8c5c989ce851524d4719640cc03
dariorojas6 <dario43055@gmail.com>
2023-04-04 20:59:08 -0300

    deploy cambios

--- src/components/weatherMainInfo.jsx
+++ src/components/weatherMainInfo.jsx
@@ -19,6 +19,6 @@ export default function WeatherMainInfo({ weather }) {
             </div>
             
         </div>
-        <iframe src={`https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d70798.25036024158!2d${weather?.location.lon}098!3d${weather?.location.lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sar!4v1680566530356!5m2!1sen!2sar`} width="100%" height="450" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src={`https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d70798.25036024158!2d${weather?.location.lon}098!3d${weather?.location.lat}!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sar!4v1680566530356!5m2!1sen!2sar`} width="100%" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 }
